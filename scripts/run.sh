7a6a44eb581b2a0d8e784d58a9a170e093fdac1b
albertgoncalves <goncalves.albert@gmail.com>
2024-01-20 16:51:33 -0500

    capture some experiments

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,6 +7,13 @@ if [ ! -d "$WD/build" ]; then
 fi
 
 cp "$1" "$WD/build/main.erl"
-erlc -Wall -Werror -o "$WD/build" "+to_core" "$WD/build/main.erl"
-erlc -o "$WD/build" -b beam "+from_core" "$WD/build/main.core"
+
+# erlc -Wall -Werror -o "$WD/build" "+to_beam" "$WD/build/main.erl"
+
+# erlc -Wall -Werror -o "$WD/build" "+to_core" "$WD/build/main.erl"
+# erlc -o "$WD/build" -b beam "+from_core" "$WD/build/main.core"
+
+erlc -Wall -Werror -o "$WD/build" -S "$WD/build/main.erl"
+erlc -o "$WD/build" -b beam "+from_asm" "$WD/build/main.S"
+
 escript "$WD/build/main.beam"
